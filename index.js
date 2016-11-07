3c0c032740008c00d2f08c1087b9fe0a223c6d35
Chris Haynes (mac) <chris.haynes@gmail.com>
2016-11-07 22:10:12 +0000

    being SSR

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const templateIndex = require('./templates/index');
 
+const Palette = require('./public/js/palette');
 const app = express();
 
 app.use(express.static('public'));
 
+const colours = [
+  [220, 16, 21],
+  [22, 167, 211],
+];
+
 app.get('/', (req, res) => {
+  if (req.query.c) {
+    const valid = Palette.parseColourText(req.query.c);
+    if (valid) {
+      let rgb;
+
+      if (valid.type === 'rgb') {
+        rgb = valid.value;
+      } else {
+        // hex
+        rgb = Palette.hexToRgb(valid.value);
+      }
+
+      colours.unshift(rgb);
+    }
+  }
+
   res.send(templateIndex({
-    colours: ['#546632'],
+    colours,
   }));
 });
 
--- templates/index.js
+++ templates/index.js
@@ -1,11 +1,50 @@
-const template = (args) => `
+const Palette = require('../public/js/palette');
+
+const template = (args) => {
+  const num = 6;
+
+  const colours = args.colours.slice(0, num).map((colour, index) => `
+    <div
+      id='sample-${num - index}'
+      class='sample'
+      data-r='${colour[0]}' data-g='${colour[1]}' data-b='${colour[2]}'
+      style='background-color: rgb(${colour[0]}, ${colour[1]}, ${colour[2]});'>
+    </div>`.trim()
+  );
+
+  for (let i = num - colours.length; i > 0; i--) {
+    colours.push(`
+      <div
+        id='sample-${i}'
+        class='sample'
+      >
+      </div>
+    `.trim());
+  }
+
+  const firstColour = colours.shift();
+
+  let rgbInfo = 'RGB';
+  let hexInfo = 'HEX';
+  let headerColour = Palette.rgbInfo([33, 150, 243]);
+  if (args.colours.length) {
+    const rgb = args.colours[0];
+
+    rgbInfo = Palette.rgbInfo(rgb);
+    hexInfo = Palette.hexInfo(rgb);
+    headerColour = rgbInfo;
+  }
+
+  return `
 <!DOCTYPE html>
 
 <html lang="en">
 
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes">
+    <meta name="viewport"
+      content="width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes"
+    >
 
     <title>Palette</title>
 
@@ -16,15 +55,15 @@ const template = (args) => `
   <body>
 
     <header class='colour-bar'>
-      <h1>Palette</h1>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <h1 style='color: ${headerColour};'>Palette</h1>
+      <div style='background-color: ${headerColour};'></div>
+      <div style='background-color: ${headerColour};'></div>
+      <div style='background-color: ${headerColour};'></div>
+      <div style='background-color: ${headerColour};'></div>
+      <div style='background-color: ${headerColour};'></div>
+      <div style='background-color: ${headerColour};'></div>
+      <div style='background-color: ${headerColour};'></div>
+      <div style='background-color: ${headerColour};'></div>
     </header>
 
     <main>
@@ -37,16 +76,26 @@ const template = (args) => `
                 <button><img src='images/touch_app.svg' alt='Take photo'></button>
             </div>
             <div class='container upload-container'>
-              <input type='file' accept='image/*' id='imagefile' class='file-upload' name='imagefile'>
+              <input
+                type='file'
+                accept='image/*'
+                id='imagefile'
+                class='file-upload'
+                name='imagefile'
+              >
               <label for="imagefile"><img src='images/file_upload.svg' alt='Upload'></label>
             </div>
-            <form class='container input-container'>
-              <input type='text' name='colour' placeholder='#hex or r,g,b'>
-              <button class='btn' type='submit'><img src='images/arrow_downward.svg' alt='Go'></button>
+            <form class='container input-container' method='get'>
+              <input type='text' name='c' placeholder='#hex / r,g,b'>
+              <button class='btn' type='submit'>
+                <img src='images/arrow_downward.svg' alt='Go'>
+              </button>
             </form>
           </div>
         </div>
-        <button class='btn btn--source'>S</button>
+        <button class='btn btn--source'>
+          <img src='images/autorenew.svg' alt='Change source'>
+        </button>
       </div>
 
       <div>
@@ -55,36 +104,27 @@ const template = (args) => `
 
       <div class='colour-info'>
         <div class='colour-data'>
-          <div class='sample-rgb'>RGB</div>
+          <div class='sample-rgb'>${rgbInfo}</div>
         </div>
-        <div class='sample-container' id='sample-container'></div>
+        <div class='sample-container' id='sample-container'>${firstColour}</div>
         <div class='colour-actions'>
-          <div class='sample-hex'>HEX</div>
+          <div class='sample-hex'>${hexInfo}</div>
         </div>
       </div>
 
-      <div class='samples-container'>${args.colours.map(colour => `<span>${colour}</span>`)}<div class='sample' id='sample-5'></div><div class='sample' id='sample-4'></div><div class='sample' id='sample-3'></div><div class='sample' id='sample-2'></div><div class='sample' id='sample-1'></div></div>
-
-      <div style='display: none;'>
-        <button class='switch'>Switch</button>
-        <button class='start'>Start</button>
-        <button class='stop'>Stop</button>
-        <button class='snap'>Snap</button>
-
-        <form>
-          <input type='text' name='colour' value='' placeholder='hex colour'>
-        </form>
-      </div>
+      <div class='samples-container'>${colours.join('')}</div>
     </main>
 
     <footer>
       <a href='https://twitter.com/lamplightdev'>@lamplightdev</a> | <a href='https://github.com/lamplightdev/palette'>github</a>
     </footer>
 
+    <script src='js/palette.js'></script>
     <script src='js/app.js'></script>
   </body>
 
 </html>
 `;
+};
 
 module.exports = template;
